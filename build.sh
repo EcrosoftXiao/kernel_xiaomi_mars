EcrosoftXiao/kernel_xiaomi_haydn

scripts: Add build scripts

build.sh
@@ -1,0 +1,41 @@
+DATE=$(date +"%Y%m%d")
+VERSION=$(git rev-parse --short HEAD)
+KERNEL_NAME=Evasi0nKernel-haydn-"$DATE"
+
+export KERNEL_PATH=$PWD
+export ANYKERNEL_PATH=~/Anykernel3
+export CLANG_PATH=~/proton-clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export SUBARCH=arm64
+
+echo "===================Setup Environment==================="
+git clone --depth=1 https://github.com/kdrag0n/proton-clang $CLANG_PATH
+git clone https://github.com/osm0sis/AnyKernel3 $ANYKERNEL_PATH
+sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh/)"
+
+echo "=========================Clean========================="
+rm -rf $KERNEL_PATH/out/ *.zip
+make mrproper && git reset --hard HEAD
+
+echo "=========================Build========================="
+make -j$(nproc) O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- haydn_defconfig
+make -j$(nproc) O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- 2>&1 | tee out/kernel.log
+
+if [ ! -e $KERNEL_PATH/out/arch/arm64/boot/Image ]; then
+    echo "=======================FAILED!!!======================="
+    exit -1>/dev/null 2>&1
+fi
+
+echo "=========================Patch========================="
+rm -r $ANYKERNEL_PATH/modules $ANYKERNEL_PATH/patch $ANYKERNEL_PATH/ramdisk
+cp $KERNEL_PATH/anykernel.sh $ANYKERNEL_PATH/
+cp $KERNEL_PATH/out/arch/arm64/boot/Image $ANYKERNEL_PATH/
+cd $ANYKERNEL_PATH
+zip -r $KERNEL_NAME *
+mv $KERNEL_NAME.zip $KERNEL_PATH/out/
+cd $KERNEL_PATH
+#rm -rf $CLANG_PATH
+rm -rf $ANYKERNEL_PATH
+echo $KERNEL_NAME.zip

EcrosoftXiao/kernel_xiaomi_mars

build.sh
@@ -1,0 +1,41 @@
+DATE=$(date +"%Y%m%d")
+VERSION=$(git rev-parse --short HEAD)
+KERNEL_NAME=Evasi0nKernel-mars-"$DATE"
+
+export KERNEL_PATH=$PWD
+export ANYKERNEL_PATH=~/Anykernel3
+export CLANG_PATH=~/proton-clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export SUBARCH=arm64
+
+echo "===================Setup Environment==================="
+git clone --depth=1 https://github.com/kdrag0n/proton-clang $CLANG_PATH
+git clone https://github.com/osm0sis/AnyKernel3 $ANYKERNEL_PATH
+sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh/)"
+
+echo "=========================Clean========================="
+rm -rf $KERNEL_PATH/out/ *.zip
+make mrproper && git reset --hard HEAD
+
+echo "=========================Build========================="
+make -j$(nproc) O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld mars_defconfig
+make -j$(nproc) O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee out/kernel.log
+
+if [ ! -e $KERNEL_PATH/out/arch/arm64/boot/Image ]; then
+    echo "=======================FAILED!!!======================="
+    exit -1>/dev/null 2>&1
+fi
+
+echo "=========================Patch========================="
+rm -r $ANYKERNEL_PATH/modules $ANYKERNEL_PATH/patch $ANYKERNEL_PATH/ramdisk
+cp $KERNEL_PATH/anykernel.sh $ANYKERNEL_PATH/
+cp $KERNEL_PATH/out/arch/arm64/boot/Image $ANYKERNEL_PATH/
+cd $ANYKERNEL_PATH
+zip -r $KERNEL_NAME *
+mv $KERNEL_NAME.zip $KERNEL_PATH/out/
+cd $KERNEL_PATH
+#rm -rf $CLANG_PATH
+rm -rf $ANYKERNEL_PATH
+echo $KERNEL_NAME.zip