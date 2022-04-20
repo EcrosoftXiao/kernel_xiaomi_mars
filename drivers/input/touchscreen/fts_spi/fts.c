04f110cbec51fc153eccc78dc320646c9eb2c9f6
EcrosoftXiao <xy19990903@gmail.com>
2022-04-20 09:49:28 +0800

    input: touchscreen: fts_spi: expose fod_pressed via sysfs

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -2785,6 +2785,13 @@ static ssize_t fts_fod_status_store(struct device *dev, struct device_attribute 
 
 	return count;
 }
+
+static ssize_t fts_fod_pressed_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+        struct fts_ts_info *info = dev_get_drvdata(dev);
+
+        return snprintf(buf, TSP_BUF_SIZE, "%d\n", info->fod_pressed);
+}
 #endif
 
 #ifdef GESTURE_MODE
@@ -2867,6 +2874,8 @@ static DEVICE_ATTR(gesture_coordinates, (S_IRUGO | S_IWUSR | S_IWGRP),
 #ifdef FTS_FOD_AREA_REPORT
 static DEVICE_ATTR(fod_status, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_fod_status_show, fts_fod_status_store);
+static DEVICE_ATTR(fod_pressed, (S_IRUGO | S_IWUSR | S_IWGRP),
+		   fts_fod_pressed_show, NULL);
 #endif
 
 #ifdef GESTURE_MODE
@@ -3562,6 +3571,7 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 
 				if ((info->sensor_sleep && !info->sleep_finger) || !info->sensor_sleep) {
 					info->fod_pressed = true;
+					sysfs_notify(&info->fts_touch_dev->kobj, NULL, dev_attr_fod_pressed.attr.name);
 					input_report_key(info->input_dev, BTN_INFO, 1);
 					input_sync(info->input_dev);
 					if (info->fod_id) {
@@ -6239,6 +6249,9 @@ static int fts_probe(struct spi_device *client)
 	error = sysfs_create_file(&info->fts_touch_dev->kobj, &dev_attr_fod_status.attr);
 	if (error)
 		logError(1, "%s ERROR: Failed to create fod_status sysfs group!\n", tag);
+	error = sysfs_create_file(&info->fts_touch_dev->kobj, &dev_attr_fod_pressed.attr);
+	if (error)
+		logError(1, "%s ERROR: Failed to create fod_pressed sysfs group!\n", tag);
 #endif
 
 #ifdef GESTURE_MODE
